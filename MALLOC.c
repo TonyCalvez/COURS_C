ae3dedf83327c6113cb49a98a99143348ef57e33
Tony CALVEZ <tony.calvez@hotmail.fr>
2018-07-08 16:33:33 +0200

    Create MALLOC.c

---
+++ MALLOC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int* memoireAllouee = NULL;
+
+    memoireAllouee = malloc(sizeof(int)); // Allocation de la mémoire
+    if (memoireAllouee == NULL)
+    {
+        exit(0); //Si l'OS refuse d'allouer la mémoire fin du programme
+    }
+
+    // Utilisation de la mémoire
+    printf("Quel age avez-vous ? ");
+    scanf("%d", memoireAllouee);
+    printf("Vous avez %d ans\n", *memoireAllouee);
+
+    free(memoireAllouee); // Libération de mémoire
+
+    return 0;
+}
+
